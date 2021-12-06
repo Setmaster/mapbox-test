2a1be22bc2b957c213b0b2abc3e9111a00d5b852
Victor <vi7org@hotmail.com>
2021-12-05 19:45:13 -0500

    Add markers

--- src/App.js
+++ src/App.js
@@ -1,32 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
-import mapboxgl from 'mapbox-gl';
-import {useEffect, useRef, useState} from "react"; // eslint-disable-line import/no-webpack-loader-syntax
+import ReactDOM from "react-dom";
+import React, { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import fetchFakeData from "./api/fetchFakeData";
+import Popup from "./components/Popup";
+import "./App.css";
 
-mapboxgl.accessToken = "pk.eyJ1Ijoic2hpdmFuaXgiLCJhIjoiY2t3cmExaHZyMHVxODMxbnljMWhhdzF3eiJ9.P9Fsyeu_1o61PHKGsTa96g";
+mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2hpdmFuaXgiLCJhIjoiY2t3cmExaHZyMHVxODMxbnljMWhhdzF3eiJ9.P9Fsyeu_1o61PHKGsTa96g";
 
-function App() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
+const App = () => {
+  const mapContainerRef = useRef(null);
+  const popUpRef = useRef(new mapboxgl.Popup({ offset: 15 }));
 
+  // initialize map when component mounts
   useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      // See style options here: https://docs.mapbox.com/api/maps/#styles
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-104.9876, 39.7405],
+      zoom: 12.5
     });
-  });
 
-  return (
-      <div>
-        <div ref={mapContainer} className="map-container" />
-      </div>
-  );
-}
+    // add navigation control (zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), "bottom-right");
+
+    map.on("load", () => {
+      // add the data source for new a feature collection with no features
+      map.addSource("random-points-data", {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: []
+        }
+      });
+      // now add the layer, and reference the data source above by name
+      map.addLayer({
+        id: "random-points-layer",
+        source: "random-points-data",
+        type: "symbol",
+        layout: {
+          // full list of icons here: https://labs.mapbox.com/maki-icons
+          "icon-image": "bakery-15", // this will put little croissants on our map
+          "icon-padding": 0,
+          "icon-allow-overlap": true
+        }
+      });
+    });
+
+    map.on("moveend", async () => {
+      // get new center coordinates
+      const { lng, lat } = map.getCenter();
+      // fetch new data
+      const results = await fetchFakeData({ longitude: lng, latitude: lat });
+      // update "random-points-data" source with new data
+      // all layers that consume the "random-points-data" data source will be updated automatically
+      map.getSource("random-points-data").setData(results);
+    });
+
+    // change cursor to pointer when user hovers over a clickable feature
+    map.on("mouseenter", "random-points-layer", e => {
+      if (e.features.length) {
+        map.getCanvas().style.cursor = "pointer";
+      }
+    });
+
+    // reset cursor to default when user is no longer hovering over a clickable feature
+    map.on("mouseleave", "random-points-layer", () => {
+      map.getCanvas().style.cursor = "";
+    });
+
+    // add popup when user clicks a point
+    map.on("click", "random-points-layer", e => {
+      if (e.features.length) {
+        const feature = e.features[0];
+        // create popup node
+        const popupNode = document.createElement("div");
+        ReactDOM.render(<Popup feature={feature} />, popupNode);
+        // set popup on map
+        popUpRef.current
+            .setLngLat(feature.geometry.coordinates)
+            .setDOMContent(popupNode)
+            .addTo(map);
+      }
+    });
+
+    // clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <div className="map-container" ref={mapContainerRef} />;
+};
 
 export default App;
