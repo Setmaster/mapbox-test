d41ad3559d4f55924b6ff15ebaf13b481e92a8b7
Victor <vi7org@hotmail.com>
2021-12-06 20:35:52 -0500

    Only show markers for locations near coords, able to delete branches

--- src/components/EndUser.js
+++ src/components/EndUser.js
@@ -1,34 +1,73 @@
 import "./enduser.css"
+import EndUserMap from "./EndUserMap";
+import {useState} from "react";
 
 export default function EndUser(props) {
+    // const [centerToCoordinates, setCenterToCoordinates] = useState([-90.29, 50.893]);
+    const [centerToLng, setCenterToLng] = useState(-90.29);
+    const [centerToLat, setCenterToLat] = useState(50.893);
+    const [lngInput, setLngInput] = useState();
+    const [latInput, setLatInput] = useState();
+
 
 
     const branchArr = Object.values(JSON.parse(localStorage.getItem('Branches')));
+    const setCoordinates = () => {
+        setCenterToLng(lngInput);
+        setCenterToLat(latInput);
+    };
 
+    const isNear = (lng, lat) => {
+        const lngDist = Math.abs(Math.abs(lng) - Math.abs(centerToLng));
+        const latDist = Math.abs(Math.abs(lat) - Math.abs(centerToLat));
+        console.log('disntace', lngDist, latDist)
+        return lngDist < 0.030 && latDist < 0.030;
+    };
 
+    const filteredBranches = branchArr.filter(branch=> isNear(branch.longitude, branch.latitude));
+    console.log('filtered',filteredBranches);
     return (
         <div className="enduser-container">
             <div className="enduser-form-container">
-                <form>
+                <form
+                onSubmit={event => {
+                    event.preventDefault();
+                    setCoordinates();
+                }}
+                >
                     <div className="enduser--controls">
-                    <div className="enduser-control">
-                        <label>Latitude</label>
-                        <input type="text"/>
-                    </div>
-                    <div className="enduser-control">
-                        <label>Latitude</label>
-                        <input type="text"/>
-                    </div>
-                        <button type="submit">Add Coordinates</button>
+                        <div className="enduser-control">
+                            <label>Longitude</label>
+                            <input type="text"
+                                   value={lngInput}
+                                   onChange={(e)=>{setLngInput(e.target.value)}}
+                            />
+                        </div>
+                        <div className="enduser-control">
+                            <label>Latitude</label>
+                            <input type="text"
+                                   value={latInput}
+                                   onChange={(e)=>{
+                                       setLatInput(e.target.value)}}
+
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                        >Add Coordinates</button>
                     </div>
                 </form>
 
             </div>
             <div className="offer-container">
                 <ul className="offer-list">
-
-                    {branchArr.map((item) => {
-                        console.log('somethin', item)
+                    <li>
+                        <EndUserMap
+                            centerToLng={centerToLng}
+                            centerToLat={centerToLat}
+                        />
+                    </li>
+                    {filteredBranches.map((item) => {
                         return <li>
                             <div className="offer-list-item">
                                 <div className="offer-list-item-img">
@@ -41,7 +80,6 @@ export default function EndUser(props) {
                                     <div className="textbox-second">
                                         <p>{item.branchName}</p>
                                     </div>
-
                                 </div>
                             </div>
                         </li>
---
+++ src/components/EndUserMap.js
@@ -0,0 +1,90 @@
+import mapboxgl from "mapbox-gl";
+import {useEffect, useRef, useState} from "react";
+
+import "./endUserMap.css"
+
+export default function EndUserMap(props) {
+
+    mapboxgl.accessToken = "pk.eyJ1Ijoic2hpdmFuaXgiLCJhIjoiY2t3cmExaHZyMHVxODMxbnljMWhhdzF3eiJ9.P9Fsyeu_1o61PHKGsTa96g";
+
+    const mapContainerRef = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-90.29);
+    const [lat, setLat] = useState(50.893);
+    const [zoom, setZoom] = useState(3);
+    const [renderedOnce, setRenderedOnce] = useState(false);
+
+
+    const easeTo = (coordinates) => {
+        if (Number.isFinite(coordinates[0]) || Number.isFinite(coordinates[1]) || coordinates[0] > 90 || coordinates[0] < -90 || coordinates[1] > 90 || coordinates[1] < -90) {
+            console.log('invalid coordinates');
+            return;
+        }
+        console.log('easing')
+        map.current.easeTo({center: coordinates, zoom: 13});
+        // map.current.flyTo({zoom:10});
+    };
+
+    //convert object-of-objects to an array - for mapping easily
+    const branchArr = Object.values(JSON.parse(localStorage.getItem('Branches')));
+
+    useEffect(() => {
+        console.log('mapccurent', map.current)
+        if (!map.current) { // initialize map only once
+            console.log('creating map')
+            map.current = new mapboxgl.Map({
+                container: mapContainerRef.current,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [lng, lat],
+                zoom: zoom
+            });
+
+
+            // Add Geolocatecontrol to the map.
+            map.current.addControl(
+                new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true
+                    },
+// When active the map will receive updates to the device's location as it changes.
+                    trackUserLocation: true,
+// Draw an arrow next to the location dot to indicate which direction the device is heading.
+                    showUserHeading: false,
+// Hide accuracy circle
+                    showAccuracyCircle: false,
+// Hide user location dot
+                    showUserLocation: false,
+                })
+            );
+            branchArr.map((eachBranch) => {
+                new mapboxgl.Marker({color: 'orange'})
+                    .setLngLat([eachBranch.longitude, eachBranch.latitude])
+                    .setPopup(new mapboxgl.Popup({offset: 30}).setHTML(`<div class="popup-img"><img src=${eachBranch.image} alt="thumbnail"/></div>
+            <div class="popup-text">
+                <h2>${eachBranch.branchName}</h2>
+                <p>${eachBranch.offer}</p>
+            </div>`))
+
+                    .addTo(map.current);
+                // markers.push(marker);
+            })
+        }
+        console.log(props.centerToLng, props.centerToLat)
+        if (renderedOnce) {
+            if (props.centerToLng && props.centerToLat) {
+                easeTo([props.centerToLng, props.centerToLat]);
+            }else{
+                console.log('Invalid coordinates');
+            }
+        } else {
+            setRenderedOnce(true);
+        }
+
+    }, [props.centerToLng, props.centerToLat])
+
+    return (
+        <div className="end-user-map-container">
+            <div ref={mapContainerRef} className="user-map"/>
+        </div>
+    )
+}
